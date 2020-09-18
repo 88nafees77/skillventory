9dcdf396d741886ed644b4bfd264e1821f3559f7
88nafees77 <contact.nafees12@gmail.com>
2020-09-18 15:37:20 +0530

    initial commit

---
+++ src/main/java/com/SkillventoryApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillventoryApplication.class, args);
+	}
+
+}
